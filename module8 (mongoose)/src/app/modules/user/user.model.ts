4b3b75f21062cb5e1f0934359076c35d151a0874
developeremdad <developeremdad@gmail.com>
2023-11-29 23:19:28 +0600

    create user

---
+++ module8 (mongoose)/src/app/modules/user/user.model.ts
@@ -0,0 +1,36 @@
+import { Schema } from 'mongoose'
+import { TUser } from './user.interface'
+
+const userModel = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'faculty', 'admin'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
